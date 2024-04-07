9eb66a7af61a26b421163e1919c8a288c44c863e
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-04-07 14:43:23 +0500

    Currency_Converter

---
+++ main.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blueBright("\n*****************WELCOME TO CURRENCY CONVERTER*****************\n"));
+const Currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "to",
+        message: "Enter to Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "amount",
+        message: "Enter Amount",
+        type: 'number'
+    }
+]);
+let fromAmount = Currency[user_answer.from]; //exchange rate
+let toAmount = Currency[user_answer.to]; //exchange rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; // usd base currency
+let convertedAmount = baseAmount * toAmount;
+console.log(chalk.greenBright(convertedAmount));
